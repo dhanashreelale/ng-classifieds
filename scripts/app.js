8f638c4774e816f52c816ec449494f97c144ca39
Dhanashree Lale <todhanashreelale@gmail.com>
2017-02-06 10:33:24 -0500

    test

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,17 @@
 /**
  * Created by laled on 1/31/17.
  */
-angular.module("ngClassifieds", []);
+    angular
+        .module('ngClassifieds', ['ngMaterial'])
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('teal')
+                .accentPalette('orange');
+        })
+        .directive("helloWorld", function () {
+            return {
+                template: "<h1>Hello, World</h1>"
+            }
+            }
+
+        );
