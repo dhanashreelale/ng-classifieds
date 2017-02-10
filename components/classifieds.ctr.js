6febe61deca48e875aac3ffb8b0cbcf47b3a199a
Dhanashree Lale <todhanashreelale@gmail.com>
2017-02-10 15:54:56 -0500

    end of lecture 27

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
 
     angular
         .module("ngClassifieds")
-        .controller("ClassifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+        .controller("ClassifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
             // $scope.name = {
             //     first:  "Dhanashree",
@@ -13,7 +13,7 @@
                 name: "Jane Doe",
                 phone: "555 555-5555",
                 email: "fakeemail@hotmail.com"
-            }
+            };
 
             classifiedsFactory.getClassifieds().then(function (remoteData) {
                  $scope.classifieds = remoteData.data;
@@ -34,13 +34,45 @@
                     $scope.classifieds.push(classified);
                     $scope.classified = {};
                     $scope.closeSidebar();
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .content("Item Added Successfully")
-                            .position('top, right')
-                            .hideDelay(3000)
-                    );
+                    $scope.showToast("Classified Saved!");
                 }
+            };
+
+            $scope.editClassified = function (classified) {
+                $scope.editing = true;
+                $scope.openSidebar();
+                $scope.classified = classified;
+            };
+
+            $scope.deleteClassified = function (event, classified) {
+                var confirm = $mdDialog.confirm()
+                    .title('Are you sure you want to delete this ' + classified.title +'? Its fancy')
+                    .ok('Yes :(')
+                    .cancel('Noooo')
+                    .targetEvent(event);
+                $mdDialog.show(confirm).then(function () {
+                    var index = $scope.classifieds.indexOf(classified);
+                    $scope.classifieds.splice(index,1);
+                }, function () {
+                    
+            })
+            };
+
+
+            $scope.saveEdit = function() {
+                $scope.editing = false;
+                $scope.classified = {};
+                $scope.closeSidebar();
+                $scope.showToast("Edit Saved!");
+            };
+
+            $scope.showToast = function(message) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content(message)
+                        .position('top, right')
+                        .hideDelay(3000)
+                )
             }
 
         })
